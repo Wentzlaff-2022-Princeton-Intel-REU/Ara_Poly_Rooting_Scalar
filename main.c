07b6896ce1b29397b4798661e6eed35688898880
Dara Oseyemi <doseyemi@princeton.edu>
2022-07-20 15:18:49 -0400

    prints poly and testing

--- main.c
+++ main.c
@@ -11,22 +11,29 @@
 
 int main() {
     double crit_conversion = 1e-14;
-    
+    printf("test 0\n");
     Polynomial_t poly;
-
     poly.degree = 2;
     double arr[3] = {1, -3, 2};
     poly.coefficients = arr;
+    printf("test 1\n");
+    printf("Polynomial:\n");
+    for (int i = poly.degree; i > 0; i++) {
+        printf("%.3lg * x^%d + ", poly.coefficients[i], i);
+    }
+    printf("%.3lg\n", poly.coefficients[0]);
 
-
+    printf("test 2\n");
     double roots [poly.degree];
+    printf("test 3\n");
     newton(poly, &roots, crit_conversion);
-
+    printf("test 4\n");
     if (roots[0] == DBL_MAX) {
         printf("Your polynomial has no roots.\n");
     }
     else {
         for (int i = 0; i < poly.degree; i++) {
+            printf("test 5\n");
             if (roots[i] == DBL_MAX) {
                 break;
             }
