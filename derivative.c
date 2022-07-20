5357b206debeac8e3d28057a69c612aaaf3d0dfe
Dara Oseyemi <doseyemi@princeton.edu>
2022-07-20 15:42:54 -0400

    ugh

--- derivative.c
+++ derivative.c
@@ -7,16 +7,10 @@
 
 /*--------------------------------------------------------------------*/
 
-Polynomial_t derivative(Polynomial_t myPoly) {
-
-    Polynomial_t differentiatedPoly;
-
-    differentiatedPoly.coefficients [myPoly.degree];   
-    differentiatedPoly.degree = myPoly.degree - 1;
+void derivative(Polynomial_t myPoly, Polynomial_t* differentiatedPoly) {
+    differentiatedPoly->degree = myPoly.degree - 1;
 
     for (int counter = myPoly.degree; counter > 0; counter--){
-        differentiatedPoly.coefficients[counter - 1] = myPoly.coefficients[counter] * counter;
+        differentiatedPoly->coefficients[counter - 1] = myPoly.coefficients[counter] * counter;
     }
-
-    return differentiatedPoly;
 }
--- newton.c
+++ newton.c
@@ -40,26 +40,35 @@ void newton(Polynomial_t poly, double* roots, double convCrit) {
 
     printf("test 2\n");
     Polynomial_t newPoly = poly;
-    Polynomial_t polyDeriv = derivative(poly);
+    Polynomial_t polyDeriv;
+    double arr[poly.degree];
+    polyDeriv.coefficients = arr;
+
+    derivative(poly, &polyDeriv);
     
     printf("test 3\n");
     for (int i = 0; i < n; i++) {
       bool firstLoop = true;
         do { 
+          printf("test 4\n");
           oldXGuess = xGuess;
           xGuess -= horner(newPoly, xGuess) / horner(polyDeriv, xGuess);
+          printf("test 5\n");
           oldDiff = diff;
           diff = fabs(xGuess - oldXGuess);
+          printf("test 6\n");
          // printf("guess: %lf, oldGuess: %lf, oldDiff: %lf, diff: %lf\n", xGuess, oldXGuess, oldDiff, diff);
 
           if (!firstLoop && diff > oldDiff && fabs(diff - oldDiff) > 1) {
             return;
           }
+          printf("test 7\n");
 
           firstLoop = false;
         } while (diff > convCrit);
+        printf("test 8\n");
         roots[i] = xGuess;
-
+        printf("test 9\n");
 
         newPoly = longDiv(newPoly, xGuess, convCrit);
         polyDeriv = derivative(newPoly);
