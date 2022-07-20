d5f091b4e86f5eebd24d0d412146486dbae803d7
Dara Oseyemi <doseyemi@princeton.edu>
2022-07-20 16:25:46 -0400

    IT WORKS!

--- main.c
+++ main.c
@@ -6,43 +6,43 @@
 #include "newton.h"
 #include "printf.h"
 #include "reading.h"
+#include "runtime.h"
 
 /*--------------------------------------------------------------------*/
 
 int main() {
     double crit_conversion = 1e-14;
-    printf("test 0\n");
+
     Polynomial_t poly;
     poly.degree = 2;
     double arr[3] = {1, 0, 1};
     poly.coefficients = arr;
-    printf("test 1\n");
+
     printf("Polynomial:\n");
     for (int i = poly.degree; i > 0; i--) {
-        printf("%.3f * x^%d + ", poly.coefficients[i], i);
+        printf("%.1f * x^%d + ", poly.coefficients[i], i);
     }
     printf("%.3f\n", poly.coefficients[0]);
 
-    printf("test 2\n");
     double roots [poly.degree];
-    printf("test 3\n");
+    start_timer();
     newton(poly, &roots, crit_conversion);
-    printf("test 4\n");
+    stop_timer();
+    int64_t cycle_time = get_timer();
+
     if (roots[0] == DBL_MAX) {
         printf("Your polynomial has no roots.\n");
     }
     else {
         for (int i = 0; i < poly.degree; i++) {
-            printf("test 5\n");
             if (roots[i] == DBL_MAX) {
-                printf("test 6\n");
                 break;
             }
             printf("The root approximation is: %.3f \n", roots[i]);
-            printf("test 7\n");
         }
     }
-    printf("test 8\n");
+
+    printf("Runtime (cycles): %d\n", cycle_time);
     
     return 0;
 }
--- newton.c
+++ newton.c
@@ -27,18 +27,15 @@ static int compare(const void * a, const void * b) {
 void newton(Polynomial_t poly, double* roots, double convCrit) {
     int n = poly.degree;
 
-    printf("test 1.0\n");
     for (int i = 0; i < n; i++) {
       roots[i] = DBL_MAX;
     }
 
-    printf("test 1.1\n");
     double xGuess = (double) rand()/ (double) rand();
     double oldXGuess = 0;
     double diff = xGuess;
     double oldDiff = 0;
 
-    printf("test 1.2\n");
     Polynomial_t newPoly = poly;
     Polynomial_t polyDeriv;
     double arr[poly.degree];
@@ -46,29 +43,22 @@ void newton(Polynomial_t poly, double* roots, double convCrit) {
 
     derivative(poly, &polyDeriv);
     
-    printf("test 1.3\n");
     for (int i = 0; i < n; i++) {
       bool firstLoop = true;
-        do { 
-          printf("test 1.4\n");
+        do { ;
           oldXGuess = xGuess;
-          xGuess -= horner(newPoly, xGuess) / horner(polyDeriv, xGuess);
-          printf("test 1.5\n");
+          xGuess -= horner(newPoly, xGuess) / horner(polyDeriv, xGuess);;
           oldDiff = diff;
-          diff = fabs(xGuess - oldXGuess);
-          printf("test 1.6\n");
+          diff = fabs(xGuess - oldXGuess);;
          // printf("guess: %lf, oldGuess: %lf, oldDiff: %lf, diff: %lf\n", xGuess, oldXGuess, oldDiff, diff);
 
           if (!firstLoop && diff > oldDiff && fabs(diff - oldDiff) > 1) {
             return;
-          }
-          printf("test 1.7\n");
+          };
 
           firstLoop = false;
         } while (diff > convCrit);
-        printf("test 1.8\n");
         roots[i] = xGuess;
-        printf("test 1.9\n");
 
         newPoly = longDiv(newPoly, xGuess, convCrit);
         derivative(newPoly, &polyDeriv);
