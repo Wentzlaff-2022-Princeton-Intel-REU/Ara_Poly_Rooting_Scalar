d8f8d62b8c6976d649eccdd1b08e33845d131da0
Dara Oseyemi <doseyemi@princeton.edu>
2022-07-21 17:59:20 -0400

    testing

--- longDiv.c
+++ longDiv.c
@@ -16,6 +16,7 @@ void longDiv(Polynomial_t* poly, double* a_n, double root, double diff) {
 
     a_n[poly->degree - 1] = poly->coefficients[poly->degree];
     for (int i = poly->degree - 1; i > 0; i--) {
+        printf("i = %d\n", i);
         a_n[i - 1] = poly->coefficients[i] + root * a_n[i];
     }
 
