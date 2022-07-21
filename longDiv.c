5bee6e89ce94d47f0ca144032b1afb951d6d8049
Dara Oseyemi <doseyemi@princeton.edu>
2022-07-21 18:30:55 -0400

    testing

--- longDiv.c
+++ longDiv.c
@@ -16,14 +16,14 @@ void longDiv(Polynomial_t* poly, double* a_n, double root, double diff) {
     printf("Inside longDiv\n");
     printPoly(*poly);
     for (int i = poly->degree; i >= 0; i--) {
-        printf("a_n[i] = %.3f, ", a_n[i]);
+        printf("poly = %.3f, ", poly->coefficients[i]);
         // poly->coefficients[i] = a_n[i];
     }
     printf("\n");
     a_n[poly->degree - 1] = poly->coefficients[poly->degree];
     for (int i = poly->degree - 1; i > 0; i--) {
         printf("poly: %.3f\n", poly->coefficients[i]);
-        printf("a_n[i] should be = %.3f\n", (poly->coefficients[i] + root * a_n[i]));
+        // printf("a_n[i] should be = %.3f\n", (poly->coefficients[i] + root * a_n[i]));
         a_n[i - 1] = poly->coefficients[i] + root * a_n[i];
     }
 
