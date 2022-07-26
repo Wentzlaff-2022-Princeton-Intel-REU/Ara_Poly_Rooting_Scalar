42ac10d971140f5abf62d6015e5f7211c5f3fa77
Mukund Ramakrishnan <mramakri@outlook.com>
2022-07-26 15:19:02 -0400

    poly change

--- main.c
+++ main.c
@@ -11,23 +11,64 @@
 /*--------------------------------------------------------------------*/
 
 int main() {
+    
     Polynomial_t poly;
     poly.degree = 2;
     double arr[] = {-1,0,1};
     poly.coefficients = arr;
 
-    double roots[poly.degree];
-    double convCrit = 1e-14;
+    for (int i = 2; i < 20; i++){
+        
+        double roots[poly.degree];
+        double convCrit = 1e-14;
+
+        start_timer();
+        newton(poly, roots, convCrit);
+        stop_timer();
+        int64_t cycleTime = get_timer();
+
+        printf("\n %d th degree Runtime (cycles): %d\n", i, cycleTime);
+
+        if (i == 3) {
+            poly.degree = 3;
+            double inForArr[] = {-5, -25, -44, -26};
+            poly.coefficients = inForArr;
+        }
+
+        if (i == 4) {
+            poly.degree = 4;
+            double inForArr[] = {3, 25, 77, 91, 28};
+            poly.coefficients = inForArr;
+        }
+
+        if (i == 5) {
+            poly.degree = 5;
+            double inForArr[] = {-2, 13, -41, 61, -44, 12};
+            poly.coefficients = inForArr;
+        }
+
+        if (i == 6) {
+            poly.degree = 6;
+            double inForArr[] = {-2, -13, 22, 208, 40, -759, -459};
+            poly.coefficients = inForArr;
+        }
+
+        
+
+    }
+   
+    // double roots[poly.degree];
+    // double convCrit = 1e-14;
 
     // printf("Polynomial:\n");
     // printPoly(poly);
     
-    start_timer();
-    newton(poly, roots, convCrit);
-    stop_timer();
-    int64_t cycleTime = get_timer();
+    // start_timer();
+    // newton(poly, roots, convCrit);
+    // stop_timer();
+    // int64_t cycleTime = get_timer();
 
-    printf("\n Runtime (cycles): %d\n", cycleTime);
+    // printf("\n Runtime (cycles): %d\n", cycleTime);
 
 
 
