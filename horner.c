a20ec4de4d3137373576206fa17ad51b1db83bb2
Mukund Ramakrishnan <mramakri@outlook.com>
2022-07-26 13:29:21 -0400

    horner change

--- horner.c
+++ horner.c
@@ -10,7 +10,7 @@ double horner(Polynomial_t poly, double guess) {
     double solution = poly.coefficients[poly.degree];
  
     for (int i = poly.degree; i > 0; i--) {
-        solution = poly.coefficients[i - 1] + (solution * guesses);
+        solution = poly.coefficients[i - 1] + (solution * guess);
     }
 
     return solution;
